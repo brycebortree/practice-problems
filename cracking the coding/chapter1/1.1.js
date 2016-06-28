eb780be184b913d5435c7d5420bdbc0b968e1d63
brytree <brycebortree@gmail.com>
2016-06-28 15:40:07 -0700

    unique string q

---
+++ cracking the coding/chapter1/1.1.js
@@ -0,0 +1,43 @@
+//is unique
+//write an algorithm that checks if a string has all unique characters
+
+// forgot the curly braces && didn't do str[j] @ first
+
+function unique(str) {
+  for(var i = 0; i < str.length; i++) {
+    for (var j = i + 1; j < str.length; j++) {
+      if(str[j]===str[i]){
+        return false;
+      }
+    }
+  }
+}
+
+unique("The brown dog");
+
+//complexity: o of n squared
+
+//is unique
+//write an algorithm that checks if a string has all unique characters
+
+// function unique(str) {
+//   //deleted unnecessary turn to array
+//   check = {};
+
+//   for(var i = 0; i < str.length; i++){
+//     //originally: str[i] != check[i]
+//     if (check[str[i]] !== null){
+//       //forgot how to assign elements
+//       check[str[i]] = 1;
+//       return false;
+//     } else {
+//       check[str[i]] = 0;
+//     }
+//   }
+//   return true;
+// }
+
+// unique("The brown dog");
+
+//got help!
+//complexity: o of n
