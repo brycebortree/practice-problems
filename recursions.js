b6bd3f4d84fc2dabfe1517bb94f5429beb411dc2
brytree <brycebortree@gmail.com>
2016-09-15 12:28:01 -0700

    adding recursions

---
+++ recursions.js
@@ -0,0 +1,35 @@
+// var factorialize = function(num){
+
+//   if (num <= 0){
+//     return 1;
+//   } else {
+//     return num * factorialize(num - 1);
+//   }
+
+// }
+// factorialize(5);
+
+// var ranger = function(num1, num2){
+
+//   if(num2 - num1 === 2){
+//     return [num1 + 1];
+//   } else {
+//     var res = ranger(num1, num2 -1);
+//     res.push(num2 - 1);
+//     return res;
+//   }
+// }
+
+// ranger(2, 9);
+
+// var adder = function(arr){
+//   if(arr.length = 1){
+//     return arr[0];
+//   } else {
+//     return arr.pop() + adder(arr);
+//   }
+
+// }
+
+// adder([1, 2, 3, 4, 5, 6]);
+
