88d697c7cba7da9a67d288af1425de6b543f6520
brytree <brycebortree@gmail.com>
2016-10-09 16:22:01 -0700

    continue working redstr

--- hackerrank/reducestring.js
+++ hackerrank/reducestring.js
@@ -1,5 +1,13 @@
 function reduceString(str){
-  
+  var finStr = '';
+  for(var i = 0; i < str.length; i++){
+    if(str[i] === str[i + 1]){
+      str = str.slice(0, acc.length - 1);
+    } else {
+      finStr += str[i];
+    }
+  }
+  return finStr;
 }
 
 //iterate through and check for doubles
