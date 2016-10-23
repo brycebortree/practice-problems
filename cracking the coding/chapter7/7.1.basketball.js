3b2ec873e09c1927026884747565f833a9c7e06c
brytree <brycebortree@gmail.com>
2016-10-23 21:35:24 +0100

    adding in ch8

---
+++ cracking the coding/chapter7/7.1.basketball.js
@@ -0,0 +1,11 @@
+// You have a basketball hoop and someonesays that you can play one of two games.
+// Game 1: Youget one shot to make the hoop.
+// Game 2: You get three shots and you have to make two of three shots.
+// If p is the probability of making a particular shot, for which values ofp should you pick one game or the other?
+
+if p < .5
+  choose game 1
+else 
+  either
+
+game two is harder to win!
